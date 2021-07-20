40772967c575d6ec7a382b93767b5613fb2467d3
koti-budati <koti.budati1994@outlook.com>
2021-07-20 19:57:42 +0530

    images added

---
+++ starbucks-reward/src/components/layouts/Navigation.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navigation = () => {
+    return (
+        <div>
+            <nav>
+                <h1>hi from Navigation</h1>
+            </nav>
+        </div>
+    )
+}
+
+export default Navigation
