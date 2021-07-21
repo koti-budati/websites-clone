518353e6c3a3ce743f4ecd6b61146b5087521d7e
koti-budati <koti.budati1994@outlook.com>
2021-07-21 06:26:43 +0530

    added dynamic titles and icons

--- starbucks-reward/src/components/pages/Rewards.js
+++ starbucks-reward/src/components/pages/Rewards.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Navigation from "../layouts/Navigation";
+import Title from "../layouts/Title";
 
 const Rewards = () => {
   return (
     <div>
+      <Title title={"Rewards"} />
       <Navigation />
     </div>
   );
