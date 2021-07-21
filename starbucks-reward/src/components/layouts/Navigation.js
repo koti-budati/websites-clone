b0f0f42b174342889adabebb4722269c7b006694
koti-budati <koti.budati1994@outlook.com>
2021-07-21 21:44:33 +0530

    updated v1.1.1

--- starbucks-reward/src/components/layouts/Navigation.js
+++ starbucks-reward/src/components/layouts/Navigation.js
@@ -1,15 +1,18 @@
 import Button from "./Button";
 import "./Navigation.css";
+import logo from "../images/logo.png";
 
 const Navigation = () => {
   return (
     <nav>
       <div id={"container"}>
-        
+        <img src={logo} alt="logo" />
         <li>menu</li>
         <li>Rewards</li>
         <li>Gift Cards</li>
-        <li id={"left"}>Find a store</li>
+        <li id={"left"}>
+          <i className={"fas fa-map-marker-alt"}></i>Find a store
+        </li>
 
         <Button text="Sign in" />
         <Button text="Join now" />
