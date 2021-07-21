d71af3447f49d9a1c448d0c61765d7eec699150a
koti-budati <koti.budati1994@outlook.com>
2021-07-21 21:34:19 +0530

    update v1.1.1

--- starbucks-reward/src/components/layouts/Navigation.js
+++ starbucks-reward/src/components/layouts/Navigation.js
@@ -1,13 +1,18 @@
+import Button from "./Button";
+import "./Navigation.css";
+
 const Navigation = () => {
   return (
     <nav>
-      <div className="container">
-        <ul>
-          <li>menu</li>
-          <li>menu</li>
-          <li>menu</li>
-          <li>menu</li>
-        </ul>
+      <div id={"container"}>
+        
+        <li>menu</li>
+        <li>Rewards</li>
+        <li>Gift Cards</li>
+        <li id={"left"}>Find a store</li>
+
+        <Button text="Sign in" />
+        <Button text="Join now" />
       </div>
     </nav>
   );
