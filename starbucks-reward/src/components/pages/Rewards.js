a8f77cd616bc78a6e99b455126f42d816d9efbc1
koti-budati <koti.budati1994@outlook.com>
2021-07-21 19:10:51 +0530

    updated v1.1

--- starbucks-reward/src/components/pages/Rewards.js
+++ starbucks-reward/src/components/pages/Rewards.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Navigation from "../layouts/navigation/Navigation";
+import Navigation from "../layouts/Navigation";
 
 const Rewards = () => {
   document.title =
