c9e204f5af332744d075dcc50f17af9250d2357f
koti-budati <koti.budati1994@outlook.com>
2021-07-22 17:36:56 +0530

    updated v1.1.2

--- starbucks-reward/src/components/layouts/Button.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./Button.css";
-
-const Button = (props) => {
-  return <button>{props.text}</button>;
-};
-
-export default Button;
--- starbucks-reward/src/components/layouts/Navigation.js
+++ starbucks-reward/src/components/layouts/Navigation.js
@@ -1,35 +1,39 @@
-import Button from "./Button";
 import "./Navigation.css";
 import logo from "../images/logo.png";
 import { Link } from "react-router-dom";
 
 const Navigation = () => {
   return (
-    <header>
-      <img src={logo} alt="logo" />
-      <nav>
-        <ul>
-          <li>
-            <Link to={"#"}>Menu</Link>
-          </li>
-          <li>
-            <Link to={"#"}>Rewards</Link>
-          </li>
-          <li>
-            <Link to={"#"}>Gift Cards</Link>
-          </li>
-        </ul>
-      </nav>
-      <Link to={"#"} id={"left"} className={"right-nav"}>
-        Find a Store
-      </Link>
-      <Link to={"#"} className={"right-nav"}>
-        <Button text={"Sign in"} />
-      </Link>
-      <Link to={"#"} className={"right-nav"}>
-        <Button text={"Join now"} />
-      </Link>
-    </header>
+    <div id={"container"}>
+      <header>
+        <img src={logo} alt="logo" />
+        <nav>
+          <ul>
+            <li>
+              <Link to={"#"}>Menu</Link>
+            </li>
+            <li>
+              <Link to={"#"}>Rewards</Link>
+            </li>
+            <li>
+              <Link to={"#"}>Gift Cards</Link>
+            </li>
+          </ul>
+        </nav>
+        <Link to={"#"} id={"left"} className={"right-nav"}>
+          Find a Store
+        </Link>
+        <Link to={"#"} className={"right-nav"}>
+          <button>Sign in</button>
+        </Link>
+        <Link to={"#"} className={"right-nav"}>
+          <button>Join now</button>
+        </Link>
+      </header>
+      <div className={"second-nav"}>
+        <h4>STARBUCKS REWARDS</h4>
+      </div>
+    </div>
   );
 };
 
