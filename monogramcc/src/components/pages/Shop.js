3a8600654c3b56ee784fd326d3f67bf4d74403d1
koti-budati <koti.budati1994@outlook.com>
2021-08-05 17:48:52 +0530

    Grid Card Update

--- monogramcc/src/components/pages/Shop.js
+++ monogramcc/src/components/pages/Shop.js
@@ -2,16 +2,17 @@ import Navigation from "../layouts/navigation/Navigation";
 import Hero from "../layouts/hero/Hero";
 import Card from "../layouts/card/Card";
 import classes from "./Shop.module.css";
+import card_1 from "../images/1.webp";
 
 const Shop = () => {
   return (
-    <div>
+    <div id={classes.shop}>
       <Navigation />
       <Hero />
       <div id={classes.card_layout}>
-        <Card img_alt="one" />
-        <Card img_alt="one" />
-        <Card img_alt="one" />
+        <Card img_alt="one" img_src={card_1}  />
+        <Card img_alt="one" img_src={card_1} />
+        <Card img_alt="one" img_src={card_1} />
       </div>
     </div>
   );
